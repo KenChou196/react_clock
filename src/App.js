a95b92a5f7fee5e46a25629a4b3cc6002d5c720a
Kay <kay@HHGs-MacBook-Air-2.local>
2022-11-03 16:16:38 +0700

    update exercise 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"; // use this to code css and make a amazing UI
+import { useEffect, useState } from "react";
 
 function App() {
+  /**
+   * init a state
+   * time : name of state => can custom with any name you like
+   * setTime: a function use to update `time`, UI use this state will update automate
+   * useState: basic hook
+   */
+  const [time, setTime] = useState(0);
+
+  /**
+   * @param effect — Imperative function that can return a cleanup function
+   * @param deps — If present, effect will only activate if the values in the list change.
+   */
+  useEffect(() => {
+    //TODO: use setinterval to update `time` by func `setTime` every 1000ms
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>time is: </div>
+      <div>{time}</div>
     </div>
   );
 }
